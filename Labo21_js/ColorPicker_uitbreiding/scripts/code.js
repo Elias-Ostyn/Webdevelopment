ac43fd646a228a184f39ce6fda90dab91924331b
Elias-Ostyn <elias.ostyn@student.vives.be>
2026-04-21 16:56:50 +0200

    aanpassing

--- Labo21_js/ColorPicker_uitbreiding/scripts/code.js
+++ Labo21_js/ColorPicker_uitbreiding/scripts/code.js
@@ -1,78 +1,115 @@
-const setup = () => {
-	let sliderRood = document.getElementsByClassName("slider1");
-	let sliderGroen = document.getElementsByClassName("slider2");
-	let sliderBlauw = document.getElementsByClassName("slider3");
+let kleurenArray = [];
 
-	sliderRood[0].addEventListener("input", update);
-	sliderGroen[0].addEventListener("input", update);
-	sliderBlauw[0].addEventListener("input", update);
+const setup = () => {
+	let sliderRood = document.getElementsByClassName("slider1")[0];
+	let sliderGroen = document.getElementsByClassName("slider2")[0];
+	let sliderBlauw = document.getElementsByClassName("slider3")[0];
+
+	sliderRood.addEventListener("input", update);
+	sliderGroen.addEventListener("input", update);
+	sliderBlauw.addEventListener("input", update);
 
 	document.getElementById("saveColor").addEventListener("click", saveColor);
-	document.getElementById("FavoKleur").addEventListener("click", favoriet);
-}
 
+	loadColors();
+};
 
+window.addEventListener("load", setup);
 
+const loadColors = () => {
+	let data = localStorage.getItem("colors");
+
+	if (data) {
+		kleurenArray = JSON.parse(data);
+
+		let laatsteKleur = kleurenArray[kleurenArray.length - 1];
+
+		if (laatsteKleur) {
+			applyColor(laatsteKleur);
+		}
+	}
+
+	opnieuwLaden();
+};
 
 const update = () => {
-	let sliderRood = document.getElementsByClassName("slider1")[0].value;
-	let sliderGroen = document.getElementsByClassName("slider2")[0].value;
-	let sliderBlauw = document.getElementsByClassName("slider3")[0].value;
+	let r = document.getElementsByClassName("slider1")[0].value;
+	let g = document.getElementsByClassName("slider2")[0].value;
+	let b = document.getElementsByClassName("slider3")[0].value;
 
-	// kleurblok aanpassen
-	let colorDemos = document.getElementsByClassName("colorDemo")[0];
-	colorDemos.style.backgroundColor = "rgb(" + sliderRood + "," + sliderGroen + "," + sliderBlauw + ")";
+	document.getElementsByClassName("colorDemo")[0].style.backgroundColor = `rgb(${r},${g},${b})`;
 
-	// waarden naast sliders tonen
-	document.getElementById("valueR").textContent = sliderRood;
-	document.getElementById("valueG").textContent = sliderGroen;
-	document.getElementById("valueB").textContent = sliderBlauw;
-}
-
+	document.getElementById("valueR").textContent = r;
+	document.getElementById("valueG").textContent = g;
+	document.getElementById("valueB").textContent = b;
+};
 
 const saveColor = () => {
 	let r = document.getElementsByClassName("slider1")[0].value;
 	let g = document.getElementsByClassName("slider2")[0].value;
 	let b = document.getElementsByClassName("slider3")[0].value;
 
-	let kleur = "rgb(" + r + "," + g + "," + b + ")";
+	let kleur = `rgb(${r},${g},${b})`;
 
+	kleurenArray.push(kleur);
+
+	localStorage.setItem("colors", JSON.stringify(kleurenArray));
+
+	applyColor(kleur);
+	opnieuwLaden();
+};
+
+const applyColor = (kleur) => {
+	let parts = kleur.match(/\d+/g);
+
+	let r = parts[0];
+	let g = parts[1];
+	let b = parts[2];
+
+	document.getElementsByClassName("slider1")[0].value = r;
+	document.getElementsByClassName("slider2")[0].value = g;
+	document.getElementsByClassName("slider3")[0].value = b;
+
+	document.getElementsByClassName("colorDemo")[0].style.backgroundColor = kleur;
+
+	document.getElementById("valueR").textContent = r;
+	document.getElementById("valueG").textContent = g;
+	document.getElementById("valueB").textContent = b;
+};
+
+const opnieuwLaden = () => {
 	let container = document.getElementsByClassName("savedColors")[0];
 
-	// maak nieuw blokje
-	let box = document.createElement("div");
-	box.classList.add("colorBox");
-	box.style.backgroundColor = kleur;
+	while (container.firstChild) {
+		container.removeChild(container.firstChild);
+	}
 
-	// delete knop
-	let deleteBtn = document.createElement("span");
-	deleteBtn.innerHTML = "x";
-	deleteBtn.classList.add("deleteBtn");
+	kleurenArray.forEach((kleur) => {
+		let box = document.createElement("div");
+		box.classList.add("colorBox");
+		box.style.backgroundColor = kleur;
 
-	deleteBtn.addEventListener("click", () => {
-		box.remove();
+		let deleteBtn = document.createElement("span");
+		deleteBtn.innerHTML = "x";
+		deleteBtn.classList.add("deleteBtn");
+
+		deleteBtn.addEventListener("click", () => {
+			verwijderKleur(kleur);
+		});
+
+		box.appendChild(deleteBtn);
+		container.appendChild(box);
 	});
+};
 
-	box.appendChild(deleteBtn);
-	container.appendChild(box);
-}
+const verwijderKleur = (kleur) => {
+	for (let i = 0; i < kleurenArray.length; i++) {
+		if (kleurenArray[i] === kleur) {
+			kleurenArray.splice(i, 1);
+			break;
+		}
+	}
 
-
-const favoriet = () => {
-		let r = document.getElementsByClassName("slider1")[0].value;
-		let g = document.getElementsByClassName("slider2")[0].value;
-		let b = document.getElementsByClassName("slider3")[0].value;
-
-		let kleur = {
-			rood: r,
-			groen: g,
-			blauw: b
-		};
-
-		localStorage.setItem("favorieteKleur", JSON.stringify(kleur));
-
-
-}
-
-
-window.addEventListener("load", setup);
+	localStorage.setItem("colors", JSON.stringify(kleurenArray));
+	opnieuwLaden();
+};
